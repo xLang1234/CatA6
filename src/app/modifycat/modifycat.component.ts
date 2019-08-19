474b615d01ea55946ab2e25e58d003cf2065b97e
ly <goh.lingyong@stee.stengg.com>
2019-08-19 18:03:38 +0800

    modifycat fix

--- src/app/modifycat/modifycat.component.ts
+++ src/app/modifycat/modifycat.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-modifycat',
   templateUrl: './modifycat.component.html',
@@ -7,9 +10,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModifycatComponent implements OnInit {
 
-  constructor() { }
-
+  formObj: FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router ) { }
+  public catsss : Array<Object>;
+  showForm = false;
   ngOnInit() {
+    
+    this.formObj = this.fb.group ({
+		  username: '',
+		  password: ''
+    });
+    this.init();
   }
+
+    
+    
+    editUserProfile = function (){
+    }
+
+    init = function (){
+        this.getCatInformation();
+    }
+
+    getCatInformation = function(){
+        this.doAJAX("read" , JSON.stringify({}), "Cat", this.callback, this);
+    }
+
+    reset = function (){
+        this.formObj = {};
+    }
+    
+
+    callback = function (arr,dis){
+        console.log("TCL: callbackUpdate -> arr", arr)
+        dis.catsss = arr;
+    }
+
+    reviewForm = function(cat){
+        this.formObj.value = cat;
+        this.showForm = true;
+    }
+
+    editCatProfile = function(){
+        delete this.formObj.value["$$hashKey"];
+        this.doAJAX("update" , JSON.stringify(this.formObj.value ) , "Cat", this.callbackUpdate , this);
+        
+    }
+
+    callbackUpdate = function (arr , dis){
+
+        dis.showForm = false;
+        alert("Successfully updated.");
+        // window.location.assign("customer.html");
+    }
+
+    
+
+
+
+  serverURL() {
+    return "http://localhost:3000/api?"; // here to change DB
+  }
+
+  doAJAXCall(partialLink, dataToSend, callback, callbackFailed ,dis ) {
+
+    let url = this.serverURL() + partialLink;
+
+    let data = dataToSend;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: data,
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function (arr) {
+            callback(arr , dis);
+            return "Success";
+        },
+        error: function (arr) {
+          callbackFailed(arr , dis);
+            return "Failed";
+        }
+    });
+
+}
+doAJAX(action , data , entity , callback ,dis ){
+    let sentString = "action=" + action + "&data=" + data + "&entity=" + entity;
+    let url = this.serverURL() + sentString;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: null,
+        success: function (arr) {
+            callback(arr , dis);
+            return "Success";
+        },
+        error: function (arr) {
+            callback(arr , dis);
+            return "Failed";
+        }
+    });
+    // $.get(url    , callback);
+}
 
 }
